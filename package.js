9bb88c069e7b071e16778e25c3b9e3aed26a4a83
Jimmie Butler <jimmiebtlr@gmail.com>
2015-08-04 17:48:24 -0500

    Bump version.

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: "elevatedevdesign:cart",
     summary: "A simple and pluggable shopping cart.",
-  	version: "0.0.1-rc.2",
+  	version: "0.0.1",
     git: "https://github.com/ElevateDev/meteor-cart.git"
 });
 
